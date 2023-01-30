651a7ac3601cc1a08a55fc4c524e3489948cc47b
Grigory Kozlov <kozlov.gr9@gmail.com>
2023-01-31 01:48:00 +0300

    Correlation stage correction

--- correlation_cognitive_methylation.py
+++ correlation_cognitive_methylation.py
@@ -1,20 +1,82 @@
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 from scipy.stats import shapiro, pearsonr, spearmanr
+from statsmodels.stats.multitest import fdrcorrection
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D 
 
-def correlation_cognitive_methylation (alpha, directory_folder, cognitive_indicator, methylation_frame, cognitive_frame):
+def scatterplot_cognitive_methylation (indicator_folder, cognitive_indicator, methylation_frame, cognitive_frame, correlation_frame):
+    cpg_name = correlation_frame.index[0]
+    color_labels = cognitive_frame['sex'].unique()
+    rgb_values = [sns.color_palette('Set1')[1], sns.color_palette('Set1')[2]]
+    color_map = dict(zip(color_labels, rgb_values))
+    
+    x, y = 15, 12
+    fig_inch = (x/2.54, y/2.54)
+    fig, ax = plt.subplots(figsize = fig_inch)
+    ax.scatter(methylation_frame[cpg_name], cognitive_frame[cognitive_indicator], c = cognitive_frame['sex'].map(color_map))
+    handles = [Line2D([0], [0], marker = 'o', color = 'w', 
+                      markerfacecolor = v, label = k, markersize = 10) for k, v in color_map.items()]
+    ax.legend(title = 'Sex:', handles = handles, bbox_to_anchor = (1.05, 1), 
+              loc = 'upper left', fontsize = 12, title_fontsize = 12)
+
+    m, b = np.polyfit(methylation_frame[cpg_name], cognitive_frame[cognitive_indicator], 1)
+    plt.plot(methylation_frame[cpg_name], m * methylation_frame[cpg_name] + b, color = sns.color_palette('Set1')[0])
+
+    ax.set_title('Correlation coef.: {0:0.2f}, p-value: {1:e}, \nregression line slope: {2:0.2f}'.format(list(correlation_frame['correlation'])[0], 
+                                                                                                         list(correlation_frame['p-value-adjusted'])[0], 
+                                                                                                         m), fontsize = 12)
+    ax.set_xlabel(cpg_name, fontsize = 12)
+    ax.set_ylabel(cognitive_indicator, fontsize = 12)
+    plt.xticks(fontsize = 10)
+    plt.yticks(fontsize = 10)
+    plt.tight_layout()
+    plt.savefig('{0}/{1}_png.png'.format(indicator_folder, cognitive_indicator), dpi = 300)
+    plt.savefig('{0}/{1}_svg.svg'.format(indicator_folder, cognitive_indicator), dpi = 300)
+    plt.savefig('{0}/{1}_tif.tif'.format(indicator_folder, cognitive_indicator), dpi = 300)
+    plt.savefig('{0}/{1}_svg.svg'.format(indicator_folder, cognitive_indicator), dpi = 300)
+    plt.close()
+    
+def genes_cpgs (indicator_folder, files, cognitive_indicator, correlation_frame):
+    cpgs_annotations = pd.read_csv(files['cpgs_annotations'], sep = '\t', index_col = 0)
+    cpgs_list1 = []
+    cpgs_list2 = []
+    cpgs_list3 = []
+    for a in correlation_frame.index:
+        cpg = cpgs_annotations['UCSC_REFGENE_NAME'][a]
+        cpgs_list1.append(cpg)
+    cpgs_list2 = [x for x in cpgs_list1 if x == x]
+    for b in cpgs_list2:
+        cpgs = b.split(';')
+        for c in cpgs:
+            if c not in cpgs_list3:
+                cpgs_list3.append(c)
+    cpgs_list_result = open('{0}/genes.txt'.format(indicator_folder), 'w')
+    for i in cpgs_list3:
+        cpgs_list_result.write(i + '\n')
+    cpgs_list_result.close()
+    
+def correlation_cognitive_methylation (alpha, files, indicator_folder, cognitive_indicator, methylation_frame, cognitive_frame):
     correlation_coef = []
     pvalues = []
-    for cpg_name in methylation_frame.columns:
+    for cpg_name in tqdm(methylation_frame.columns, ncols = 100):
         corr = spearmanr(cognitive_frame[cognitive_indicator], methylation_frame[cpg_name])[0]
         correlation_coef.append(corr)  
         pval = spearmanr(cognitive_frame[cognitive_indicator], methylation_frame[cpg_name])[1]
         pvalues.append(pval)
-    correlation_frame = pd.DataFrame({'cpg': methylation_frame.columns})
-    correlation_frame.loc[:, 'p-value'] = pvalues
-    correlation_frame.loc[:, 'correlation'] = correlation_coef
-    #correlation_frame = correlation_frame.set_index('cpg')
-    
+    correlation_frame = pd.DataFrame({'cpg': methylation_frame.columns, 'correlation': correlation_coef, 'p-value': pvalues})
+    correlation_frame = correlation_frame.set_index('cpg') 
     correlation_frame = correlation_frame.loc[(correlation_frame['p-value'] < alpha)]
     correlation_frame = correlation_frame.sort_values(by = 'p-value', ascending = True)
-    correlation_frame.to_csv('{0}/{1}_correlation_frame.txt'.format(directory_folder, cognitive_indicator), sep = '\t', index = True)
+    
+    pvalue_adjusted = fdrcorrection(correlation_frame['p-value'], alpha = alpha, method = 'indep', is_sorted = True)
+    correlation_frame['p-value-adjusted'] = pvalue_adjusted[1]
+    correlation_frame = correlation_frame.loc[(correlation_frame['p-value-adjusted'] < alpha)]
+    scatterplot_cognitive_methylation (indicator_folder, cognitive_indicator, 
+                                       methylation_frame, cognitive_frame, correlation_frame)
+    genes_cpgs (indicator_folder, files, cognitive_indicator, correlation_frame)
+    
+    correlation_frame.to_csv('{0}/correlation_frame.txt'.format(indicator_folder), sep = '\t', index = True)
     return correlation_frame
--- data_preproc.py
+++ data_preproc.py
@@ -2,24 +2,25 @@ import pandas as pd
 import numpy as np
 import os
 
-def data_preproc (cognitive_indicator):
-    GSE52588_values = np.load('D:/unn/GSE52588/GSE52588_beta_fn.npz', 'r')['X'][0:29]
-    GSE52588_names = np.load('D:/unn/GSE52588/GSE52588_beta_fn.npz', 'r')['all_cpg_names']
-    GSE52588_samples = list(pd.read_csv('D:/unn/GSE52588/GSE52588_samples.txt', sep = '\t')['sample_name'][0:29])
-    methylation_frame = pd.DataFrame(GSE52588_values, index = GSE52588_samples, columns = GSE52588_names)
-
-    cognitive_frame = pd.DataFrame(pd.read_csv('D:/unn/GSE52588/DOWN_FENOTIPO_No4,8,12_PerCorrelazioni.tsv', 
-                                           sep = '\t', index_col = 0)[cognitive_indicator])
+def data_preproc (files, directory_research, cognitive_indicator):
+    indicator_folder = directory_research + '/' + cognitive_indicator
+    if not os.path.isdir(indicator_folder):
+        os.mkdir(indicator_folder)
+        
+    cpg_values = np.load(files['cpg_values'], 'r')['X'][0:29]
+    cpg_names = np.load(files['cpg_names'], 'r')['all_cpg_names']
+    data_samples = list(pd.read_csv(files['data_samples'], sep = '\t')['sample_name'][0:29])
+    data_sex = list(pd.read_csv(files['data_samples'], sep = '\t')['gender'][0:29])
+    cognitive_frame = pd.DataFrame(pd.read_csv(files['cognitive_frame'], sep = '\t', index_col = 0)[cognitive_indicator])
+    
+    methylation_frame = pd.DataFrame(cpg_values, index = data_samples, columns = cpg_names)
+    cognitive_frame['sex'] = data_sex
+    
     nan_index = cognitive_frame.index[pd.isnull(cognitive_frame[cognitive_indicator])]
-    for i in nan_index:
-        cognitive_frame.drop([i], inplace = True)
-        methylation_frame.drop([i], inplace = True)
+    for nan in nan_index:
+        cognitive_frame.drop([nan], inplace = True)
+        methylation_frame.drop([nan], inplace = True)
 
-    directory_folder = 'D:/unn/test/{}'.format(cognitive_indicator)
-    if not os.path.isdir(directory_folder):
-        os.mkdir(directory_folder)
-    methylation_frame.to_csv('D:/unn/test/{0}/{1}_methylation_frame.txt'.format(cognitive_indicator, cognitive_indicator), 
-                             sep = '\t', index = True)
-    cognitive_frame.to_csv('D:/unn/test/{0}/{1}_cognitive_frame.txt'.format(cognitive_indicator, cognitive_indicator), 
-                           sep = '\t', index = True)
-    return directory_folder, methylation_frame, cognitive_frame
+    methylation_frame.to_csv('{0}/methylation_frame.txt'.format(indicator_folder), sep = '\t', index = True)
+    cognitive_frame.to_csv('{0}/cognitive_frame.txt'.format(indicator_folder), sep = '\t', index = True)
+    return methylation_frame, cognitive_frame, indicator_folder
