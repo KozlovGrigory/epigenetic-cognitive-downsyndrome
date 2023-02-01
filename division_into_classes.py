e40ded66651fcbb50dd8ce1c0e7b3bcca53ae956
Grigory Kozlov <kozlov.gr9@gmail.com>
2023-02-01 03:52:21 +0300

    Division into classes (version 2)

--- division_into_classes.py
+++ division_into_classes.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
 
-def division_boundary_values (lim1, lim2, lim3, cognitive_frame, cognitive_indicator):
+def division_boundary_values (cognitive_indicator, cognitive_frame, class_values, lim1 = 0, lim2 = 0):
+    if lim1 == 0 and lim2 == 0:
+        splits = np.array_split(class_values, 3)
+        lim1 = splits[0][-1]
+        lim2 = splits[1][-1]
+    
     classes = pd.DataFrame({'value': list(cognitive_frame[cognitive_indicator]), 'class': 0})
     for i in range(len(classes['value'])):
         if classes['value'].loc[i] <= lim1:
@@ -10,34 +15,23 @@ def division_boundary_values (lim1, lim2, lim3, cognitive_frame, cognitive_indic
             classes.loc[i, 'class'] = 1
         else:
             classes.loc[i, 'class'] = 2
-    classes_equal = []  
-    classes_equal.extend([list(classes['class']).count(0),
-                        list(classes['class']).count(1),
-                        list(classes['class']).count(2)])
-    return classes, classes_equal
+    classes_members = [list(classes['class']).count(0),
+                       list(classes['class']).count(1),
+                       list(classes['class']).count(2)]
+    return classes, classes_members
 
-def division_into_classes (cognitive_indicator, cognitive_frame, user_choice, 
-                           lim1, lim2, lim3):
-    boundary_values_all = []
-    values = sorted(list(cognitive_frame[cognitive_indicator]))
-    lim1 = values[len(values) // 3]
-    lim2, lim3 = values[len(values) // 3 * 2], values[len(values) - 1]
-    boundary_values_all.extend([lim1, lim2, lim3])
-    classes_all, classes_all_equal = division_boundary_values(lim1, lim2, lim3, cognitive_frame, cognitive_indicator)
-    
-    boundary_values_unique = []
-    values = sorted(list(set(cognitive_frame[cognitive_indicator])))
-    lim1 = values[len(values) // 3]
-    lim2, lim3 = values[len(values) // 3 * 2], values[len(values) - 1] 
-    boundary_values_unique.extend([lim1, lim2, lim3])
-    classes_unique, classes_unique_equal = division_boundary_values(lim1, lim2, lim3, cognitive_frame, cognitive_indicator)
-    
-    if user_choice == 0:
-        return boundary_values_all, boundary_values_unique, classes_all, classes_unique, classes_all_equal, classes_unique_equal
-    elif user_choice == 1:
-        return classes_all
-    elif user_choice == 2:
-        return classes_unique  
-    elif user_choice == 3:
-        classes_user, classes_user_equal = division_boundary_values(lim1, lim2, lim3, cognitive_frame, cognitive_indicator)
+def division_into_classes (cognitive_indicator, cognitive_frame, division_method = 0, lim1 = 0, lim2 = 0):
+    if division_method != 3:
+        class_values = sorted(list(cognitive_frame[cognitive_indicator]))
+        classes_all, classes_all_members = division_boundary_values(cognitive_indicator, cognitive_frame, class_values)
+        if division_method == 1:
+            return classes_all
+        class_values = sorted(list(set(cognitive_frame[cognitive_indicator])))
+        classes_unique, classes_unique_members = division_boundary_values(cognitive_indicator, cognitive_frame, class_values)
+        if division_method == 2:
+            return classes_unique 
+        return classes_all_members, classes_unique_members
+    else:
+        class_values = sorted(list(cognitive_frame[cognitive_indicator]))
+        classes_user, classes_user_members = division_boundary_values(cognitive_indicator, cognitive_frame, class_values, lim1, lim2)
         return classes_user 
